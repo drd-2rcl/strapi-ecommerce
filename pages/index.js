e2f90c2d9d1be4f1a0af5e6b078d1d55539f785e
drd-2rcl <drd.rclrd@gmail.com>
2018-10-18 17:22:29 -0300

    commit init

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Button, Alert } from "reactstrap";
+
+
+export default () => {
+  return (
+    <div>
+    <div>
+      <Alert color= "primary">
+      Whatsup!
+      </Alert>
+      &nbsp; <Button color="primary" />
+    </div>
+  </div>
+  )
+}
